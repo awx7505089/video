8dd413b72502d2b9833849e407f0560a3bcaa421
awx7505089 <a401521765@163.com>
2025-09-02 13:10:03 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        toutiao: {
+          red: '#F04142',
+          black: '#222222',
+          gray: '#F8F8F8',
+          light: '#999999',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
